840554894115b217cc736ad680e8cb89c94cef79
Sivan Kaplan <sivanagar@gmail.com>
2022-01-13 23:46:30 -0800

    added project

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,45 +1,51 @@
 import React, { useState } from 'react';
 import Project from '../Project';
-import {Container , CardGroup} from 'react-bootstrap/';
+import { Container, CardGroup } from 'react-bootstrap/';
 
 function Portfolio() {
   const [projects] = useState([
     {
+      title: 'Cache',
+      imageSrc: 'cache.png',
+      github: 'https://github.com/sivanagar/garage-finder',
+      link: 'https://cache-project.herokuapp.com/',
+    },
+    {
       title: 'Giftby',
       imageSrc: 'giftby.jpg',
       github: 'https://github.com/sivanagar/theNextGen',
-      link: 'https://secret-santa-project.herokuapp.com/'
+      link: 'https://secret-santa-project.herokuapp.com/',
     },
     {
       title: 'National Park Search',
       imageSrc: 'NationalParkSearch.png',
       github: 'https://github.com/sivanagar/NationalParkSearch',
-      link: 'https://sivanagar.github.io/NationalParkSearch/'
+      link: 'https://sivanagar.github.io/NationalParkSearch/',
     },
     {
       title: 'Weather Dashboard',
       imageSrc: 'weatherDashboard.png',
       github: 'https://github.com/sivanagar/WeatherDashboard',
-      link: 'https://sivanagar.github.io/WeatherDashboard/'
+      link: 'https://sivanagar.github.io/WeatherDashboard/',
     },
     {
       title: 'Team Webpage Generator',
       imageSrc: 'team.png',
       github: 'https://github.com/sivanagar/TeamProfileGenerator',
-      link: 'https://github.com/sivanagar/TeamProfileGenerator'
-    }
+      link: 'https://github.com/sivanagar/TeamProfileGenerator',
+    },
   ]);
 
   return (
     <Container>
       <h2 className="heading">Portfolio</h2>
       <CardGroup>
-      {projects.map(project => (
-        <Project currentProject={project} key={project.title}></Project>
-      ))}
+        {projects.map((project) => (
+          <Project currentProject={project} key={project.title}></Project>
+        ))}
       </CardGroup>
     </Container>
-  )
-};
+  );
+}
 
 export default Portfolio;
